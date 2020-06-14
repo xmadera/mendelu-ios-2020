0c3279e9822d6d1baf7ab88201894c37c1a0bc92
xmadera <xmadera@mendelu.cz>
2020-06-14 18:11:04 +0200

    Detail edit

--- KinoCircle/Views/MoviesDetail/MoviesDetailCDView.swift
+++ KinoCircle/Views/MoviesDetail/MoviesDetailCDView.swift
@@ -18,122 +18,148 @@ struct MoviesDetailCDView: View {
     @ObservedObject private var favoritesListVM = FavoritesListViewModel()
     
     var body: some View {
-        VStack {
-            VStack() {
-                ZStack{
-                    if self.favoritesListVM.movie != nil {
-                        URLImage((self.favoritesListVM.movie!.poster!.getImageUrl)! , delay: 0.25, content:  {
-                            $0.image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(height: 200)
-                        })
-                    }
-                    
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .background(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .top, endPoint: .bottom))
-                        .cornerRadius(0)
-                        .frame(height: 200)
-                    
-                    
-                    Text(self.favoritesListVM.movie?.plot ?? "")
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.leading)
-                        .padding()
-                        .background(Color("textDarkBackground"))
-                        .cornerRadius(5)
-                        .offset(y: 170)
-                    
-                    Text(self.favoritesListVM.movie?.genre ?? "")
-                        .foregroundColor(.white)
-                        .font(.system(size: 12, weight: .regular))
-                        .padding()
-                        .background(Color("textBlueBackground"))
-                        .frame(height: 40)
-                        .cornerRadius(20)
-                        .offset(y: 278)
-                    
-                    HStack {
-                        Text(self.favoritesListVM.movie?.title ?? "")
-                            .foregroundColor(.white)
-                            .font(.system(size: 24, weight: .regular))
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            self.showingActionSheet = true;
-                        }) {
-                            Image(systemName: "heart.fill").font(.system(size: 24))
-                                .foregroundColor(.white)
-                        } .alert(isPresented: $showingActionSheet) {
-                            Alert(title: Text("Are you sure you want to remove this from your favorites?"), message: Text("Your review will be lost"), primaryButton: .destructive(Text("Remove")) {
-                                MovieCD.delete(context: self.viewContext, movie: (self.favoritesListVM.movie!) as MovieCD)
-                                }, secondaryButton: .cancel())
+        ZStack {
+            VStack {
+                ZStack {
+                    GeometryReader { geometry in
+                        if self.favoritesListVM.movie != nil {
+                            URLImage((self.favoritesListVM.movie!.poster!.getImageUrl)! , delay: 0.25, content:  {
+                                $0.image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geometry.size.width, height: geometry.size.height / 3)
+                            })
                         }
                         
-                    }.padding()
-                        .padding(.top)
-                }.edgesIgnoringSafeArea(.top)
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .background(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .top, endPoint: .bottom))
+                            .cornerRadius(0)
+                            .frame(width: geometry.size.width, height: geometry.size.height / 3)
+                    }
+                }
+                Spacer()
             }
-            
             Spacer()
+            VStack {
+                Spacer()
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            Text(self.favoritesListVM.movie?.genre ?? "")
+                                .foregroundColor(.blue)
+                                .font(.system(size: 12, weight: .bold))
+                                .padding(.bottom)
+                            
+                            Text("Director: \(self.favoritesListVM.movie?.director ?? "")")
+                                .font(.system(size: 14, weight: .bold))
+                                .padding(.bottom)
+                            
+                            Text(self.favoritesListVM.movie?.plot ?? "")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }.padding(.horizontal)
+                            .padding(.top)
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading) {
+                            Text("Actors:")
+                                .font(.system(size: 14, weight: .regular))
+                                .padding(.bottom)
+                            
+                            Text(self.favoritesListVM.movie?.actors ?? "")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(.gray)
+                        }.padding(.horizontal)
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("My review:")
+                                    .font(.system(size: 14, weight: .regular))
+                                
+                                Button(action: {
+                                    self.showingEditReview = true
+                                }) { Text("edit")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(.horizontal, 8)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.gray, lineWidth: 1)
+                                    )
+                                }.padding(.leading, 4)
+                                    .sheet(isPresented: $showingEditReview, content: { EditReviewView(showingEditReview: self.$showingEditReview) })
+                            }.padding(.bottom)
+                            Text("The best movie I'v ever seen")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(.gray)
+                                .padding(.bottom)
+                        }.padding(.horizontal)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Ratings:")
+                                .font(.system(size: 14, weight: .regular))
+                                .padding(.bottom)
+                            HStack {
+                                VStack {
+                                    Text("Metascore")
+                                    Text("69")
+                                }.foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(8)
+                                    .background(Color("textBlueBackground"))
+                                    .padding(.trailing, 8)
+                                VStack {
+                                    Text("IMDb")
+                                    Text("8.0")
+                                }.foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(8)
+                                    .background(Color("textBlueBackground"))
+                                    .padding(.trailing, 8)
+                                VStack {
+                                    Text("My rating")
+                                    Text("9.0")
+                                }.foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(8)
+                                    .background(Color("textBlueBackground"))
+                                    .padding(.trailing, 8)
+                            }
+                        }.padding(.horizontal)
+                        
+                        
+                        Spacer()
+                    }.background(Color.white)
+                        .cornerRadius(20)
+                        .frame(height: 450)
+                        .shadow(color: Color.black, radius: 10, x: 0, y: 0)
+                    
+                }
+            }.edgesIgnoringSafeArea(.bottom)
             
-            VStack(alignment: .leading) {
-                Text("Director: \(self.favoritesListVM.movie?.director ?? "")")
-                    .font(.system(size: 14, weight: .bold))
-                    .padding(.bottom)
-                
-                
-                Text("Writers:")
-                    .font(.system(size: 14, weight: .regular))
-                Text(self.favoritesListVM.movie?.writer ?? "")
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(.vertical)
-                
-                Text("Actors:")
-                    .font(.system(size: 14, weight: .regular))
-                Text(self.favoritesListVM.movie?.actors ?? "")
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(.vertical)
-                
-                Text("My review:")
-                    .font(.system(size: 14, weight: .regular))
-                Text("...")
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(.vertical)
-                
-                Divider()
-                
-                Text("Ratings")
-                    .padding(.vertical)
-                
-                //                HStack {
-                //                    ForEach(self.movieDetailVM.movie.ratings) { rating in
-                //                        VStack {
-                //                            Text("\(rating.Source)")
-                //                            Text("\(rating.Value)")
-                //                        }
-                //                    }
-                //                }
-                
-            }.padding()
+        }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarTitle(Text(self.favoritesListVM.movie?.title ?? ""), displayMode: .large)
+        .navigationBarItems(trailing:
             
-            
-            
-        }.navigationBarItems(trailing:
             Button(action: {
-                self.showingEditReview = true
-            }) { Image(systemName: "list.bullet")
-            }.sheet(isPresented: $showingEditReview, content: { EditReviewView(showingEditReview: self.$showingEditReview) }))
+                self.showingActionSheet = true;
+            }) {
+                Image(systemName: "heart.fill").font(.system(size: 24))
+                    .foregroundColor(.white)
+            } .alert(isPresented: $showingActionSheet) {
+                Alert(title: Text("Are you sure you want to remove this from your favorites?"), message: Text("Your review will be lost"), primaryButton: .destructive(Text("Remove")) {
+                    MovieCD.delete(context: self.viewContext, movie: (self.favoritesListVM.movie!) as MovieCD)
+                    }, secondaryButton: .cancel())}
+        )
             .onAppear{
                 self.favoritesListVM.loadMovie(id: self.movieId)
         }
-        
     }
     
     func isInCoreData(id: String) -> Bool {
--- KinoCircle/Views/MoviesDetail/MoviesDetailView.swift
+++ KinoCircle/Views/MoviesDetail/MoviesDetailView.swift
@@ -9,7 +9,6 @@
 import SwiftUI
 import URLImage
 
-
 struct MoviesDetailView: View {
     @Environment(\.managedObjectContext) var viewContext
     @State var movieId: String
@@ -18,119 +17,138 @@ struct MoviesDetailView: View {
     @ObservedObject private var favoritesListVM = FavoritesListViewModel()
     
     var body: some View {
-        VStack {
-            VStack(alignment: .center) {
-                ZStack{
-                    URLImage((self.movieDetailVM.movie.poster.getImageUrl)!, delay: 0.25, content:  {
-                        $0.image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(height: 200)
-                    })
-                    
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .background(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .top, endPoint: .bottom))
-                        .cornerRadius(0)
-                        .frame(height: 200)
-                    
-                    
-                    Text(self.movieDetailVM.movie.plot)
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.leading)
-                        .padding()
-                        .background(Color("textDarkBackground"))
-                        .cornerRadius(5)
-                        .offset(y: 170)
-                    
-                    Text(self.movieDetailVM.movie.genre)
-                        .foregroundColor(.white)
-                        .font(.system(size: 12, weight: .regular))
-                        .padding()
-                        .background(Color("textBlueBackground"))
-                        .frame(height: 40)
-                        .cornerRadius(20)
-                        .offset(y: 278)
-                    
-                    HStack {
-                        Text(self.movieDetailVM.movie.title)
-                            .foregroundColor(.white)
-                            .font(.system(size: 24, weight: .regular))
+        ZStack {
+            VStack {
+                ZStack {
+                    GeometryReader { geometry in
+                        URLImage((self.movieDetailVM.movie.poster.getImageUrl)!, delay: 0.25, content:  {
+                            $0.image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: geometry.size.width, height: geometry.size.height / 3)
+                        })
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .background(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .top, endPoint: .bottom))
+                            .cornerRadius(0)
+                            .frame(width: geometry.size.width, height: geometry.size.height / 3)
+                    }
+                }
+                Spacer()
+            }
+            Spacer()
+            VStack {
+                Spacer()
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            Text(self.movieDetailVM.movie.genre)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 12, weight: .bold))
+                                .padding(.bottom)
+                            
+                            Text("Director: \(self.movieDetailVM.movie.director)")
+                                .font(.system(size: 14, weight: .bold))
+                                .padding(.bottom)
+                            
+                            Text(self.movieDetailVM.movie.plot)
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }.padding(.horizontal)
+                            .padding(.top)
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading) {
+                            Text("Actors:")
+                                .font(.system(size: 14, weight: .regular))
+                                .padding(.bottom)
+                            
+                            Text(self.movieDetailVM.movie.actors)
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(.gray)
+                        }.padding(.horizontal)
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading) {
+                            Text("My review:")
+                                .font(.system(size: 14, weight: .regular))
+                                .padding(.bottom)
+                            Text("The best movie I'v ever seen")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(.gray)
+                                .padding(.bottom)
+                        }.padding(.horizontal)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Ratings:")
+                                .font(.system(size: 14, weight: .regular))
+                                .padding(.bottom)
+                            HStack {
+                                VStack {
+                                    Text("Metascore")
+                                    Text("69")
+                                }.foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(8)
+                                    .background(Color("textBlueBackground"))
+                                    .padding(.trailing, 8)
+                                VStack {
+                                    Text("IMDb")
+                                    Text("8.0")
+                                }.foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(8)
+                                    .background(Color("textBlueBackground"))
+                                    .padding(.trailing, 8)
+                                VStack {
+                                    Text("My rating")
+                                    Text("9.0")
+                                }.foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .regular))
+                                    .padding(8)
+                                    .background(Color("textBlueBackground"))
+                                    .padding(.trailing, 8)
+                            }
+                        }.padding(.horizontal)
+                        
                         
                         Spacer()
-                        Button(action: {
-                            if !self.isInCoreData(id: self.movieDetailVM.movie.imdbID) {
-                                self.showingActionSheet = true;
-                            }
-                        }) {
-                            if !isInCoreData(id: self.movieDetailVM.movie.imdbID) {
-                                Image(systemName: "heart").font(.system(size: 24))
-                                    .foregroundColor(.white)
-                            } else {
-                                Image(systemName: "heart.fill").font(.system(size: 24))
-                                    .foregroundColor(.white)
-                            }
-                            
-                        }.alert(isPresented: $showingActionSheet) {
-                            Alert(title: Text("Are you sure you want to add this from your favorites?"), primaryButton: .default(Text("Add")) {
-                                MovieCD.create(context: self.viewContext, movie: self.movieDetailVM.movie)
-                                self.favoritesListVM.loadMovies(tag: self.movieDetailVM.movie.type)
-                                }, secondaryButton: .cancel())
-                        }
-                        
-                    }.padding()
-                        .padding(.top)
-                }.edgesIgnoringSafeArea(.top)
+                    }.background(Color.white)
+                        .cornerRadius(20)
+                        .frame(height: 450)
+                        .shadow(color: Color.black, radius: 10, x: 0, y: 0)
+                    
+                }
+            }.edgesIgnoringSafeArea(.bottom)
+            
+        }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarTitle(Text(self.movieDetailVM.movie.title), displayMode: .large)
+        .navigationBarItems(trailing: Button(action: {
+            if !self.isInCoreData(id: self.movieDetailVM.movie.imdbID) {
+                self.showingActionSheet = true;
+            }
+        }) {
+            if !isInCoreData(id: self.movieDetailVM.movie.imdbID) {
+                Image(systemName: "heart").font(.system(size: 24))
+                    .foregroundColor(.white)
+            } else {
+                Image(systemName: "heart.fill").font(.system(size: 24))
+                    .foregroundColor(.white)
             }
             
-            Spacer()
-            
-            VStack(alignment: .leading) {
-                Text("Director: \(self.movieDetailVM.movie.director)")
-                    .font(.system(size: 14, weight: .bold))
-                    .padding(.bottom)
-                
-                
-                Text("Writers:")
-                    .font(.system(size: 14, weight: .regular))
-                Text(self.movieDetailVM.movie.writer)
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(.vertical)
-                
-                Text("Actors:")
-                    .font(.system(size: 14, weight: .regular))
-                Text(self.movieDetailVM.movie.actors)
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(.vertical)
-                
-                Text("My review:")
-                    .font(.system(size: 14, weight: .regular))
-                Text("...")
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(.vertical)
-                
-                Divider()
-                
-                Text("Ratings")
-                    .padding(.vertical)
-                
-                //                HStack {
-                //                    ForEach(self.movieDetailVM.movie.ratings) { rating in
-                //                        VStack {
-                //                            Text("\(rating.Source)")
-                //                            Text("\(rating.Value)")
-                //                        }
-                //                    }
-                //                }
-                
-            }.padding()
-        }
-        .onAppear{
-            self.movieDetailVM.loadMovie(imdbID: self.movieId)
+        }.alert(isPresented: $showingActionSheet) {
+            Alert(title: Text("Are you sure you want to add this from your favorites?"), primaryButton: .default(Text("Add")) {
+                MovieCD.create(context: self.viewContext, movie: self.movieDetailVM.movie)
+                self.favoritesListVM.loadMovies(tag: self.movieDetailVM.movie.type)
+                }, secondaryButton: .cancel())
+        })
+            .onAppear{
+                self.movieDetailVM.loadMovie(imdbID: self.movieId)
         }
     }
     func isInCoreData(id: String) -> Bool {
@@ -138,8 +156,3 @@ struct MoviesDetailView: View {
     }
 }
 
-//struct MoviesDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/movieDetailVM
-//    }
-//}
